3a8b448c6cfd62881f77e1e584f6b915200b8d69
Octaver <Octaver2131@outlook.com>
2025-07-06 22:50:54 +0800

    redis 序列化问题解决

--- src/main/java/com/yupi/usercenter/config/RedisTemplateConfig.java
+++ src/main/java/com/yupi/usercenter/config/RedisTemplateConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
@@ -14,6 +15,9 @@ public class RedisTemplateConfig {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(RedisSerializer.string());
+
+        GenericJackson2JsonRedisSerializer jsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+        redisTemplate.setValueSerializer(jsonRedisSerializer);
         return redisTemplate;
     }
 }
