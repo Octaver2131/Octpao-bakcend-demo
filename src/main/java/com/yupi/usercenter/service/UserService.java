51818967ad0a3b0824131410bde83a9948289ea5
Octaver <Octaver2131@outlook.com>
2025-08-27 10:48:11 +0800

    匹配用户功能

--- src/main/java/com/yupi/usercenter/service/UserService.java
+++ src/main/java/com/yupi/usercenter/service/UserService.java
@@ -1,7 +1,9 @@
 package com.yupi.usercenter.service;
 
+import com.yupi.usercenter.common.BaseResponse;
 import com.yupi.usercenter.model.domain.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.usercenter.model.vo.UserVO;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -96,4 +98,13 @@ public interface UserService extends IService<User> {
      * @return
      */
     boolean isAdmin(User loginUser);
+
+    /**
+     * 匹配用户
+     *
+     * @param num
+     * @param loginUser
+     * @return
+     */
+    List<User> matchUsers(long num, User loginUser);
 }
